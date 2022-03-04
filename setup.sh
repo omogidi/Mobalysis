cf299a6e09a8fdb711ca10eb3a2dcbf0b2ae076e
unknown <alaoemmanuel2014@gmail.com>
2022-03-04 20:59:27 +0100

    Task 4

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ sudo -u postgresql psql -c "CREATE USER mob_db_user PASSWORD 'mob_db_pass'"
 \q
 exit
 sudo useradd mob_app_usr -p 'Project4'
+sudo apt install -y python3-venv
+sudo -u postgres psql -c "create database mobalytics"
+sudo -u postgres psql -c "ALTER DATABASE mobalytics OWNER TO mob_db_user"
