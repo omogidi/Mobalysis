35ff231b704ad160774471dee6e6b93874e76ce0
unknown <alaoemmanuel2014@gmail.com>
2022-03-04 21:12:05 +0100

    Task 6 - Update install.sh to:

--- install.sh
+++ install.sh
@@ -7,3 +7,15 @@ sudo echo “export DBPASS=“mob_db_pass”” >> /home/mob_app_usr/.bashrc
 sudo echo “export DBHOST=“localhost”” >> /home/mob_app_usr/.bashrc
 sudo echo “export DBPORT=“5432”” >> /home/mob_app_usr/.bashrc
 source /home/mob_app_usr/.bashrc
+sudo python3 -m pip install --user --upgrade pip
+sudo python3 -m pip --version
+sudo python3 -m pip install --user virtualenv
+sudo python3 -m venv /home/mob_app_usr/env
+
+source /home/mob_app_usr/env/bin/activate
+
+sudo python3 -m pip install django
+
+sudo python3 ~/Mobalysis/backend/manage.py makemigrations
+
+sudo python3 ~/Mobalysis/backend/manage.py migrate
